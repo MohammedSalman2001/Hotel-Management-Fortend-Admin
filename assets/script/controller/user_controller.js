126c39019dbeabbbaef22d80ce5413690f827792
MohammedSalman2001 <salmannisthar123@gmail.com>
2023-10-29 16:11:09 +0530

    userSearch complete

--- assets/script/controller/user_controller.js
+++ assets/script/controller/user_controller.js
@@ -7,7 +7,56 @@ export class UserController {
             this.saveUser()
         })
 
+        $('#search').keydown(e => {
+            if (e.keyCode == 13) {
+                e.preventDefault();
+                let customerMail = $('#search').val();
+                console.log(customerMail)
+                this.searchCustomer(customerMail)
+            }
+
+        });
+
+
+
     }
+
+    searchCustomer(email){
+        var settings = {
+            "url": "http://localhost:8081/api/v1/user/0/" + email,
+            "method": "GET",
+            "timeout": 0,
+            "headers": {
+                "Authorization": "Bearer " + getKey()
+            },
+        };
+
+        $.ajax(settings).done(function (responseData) {
+
+            $("#email").prop('readonly', true);
+            console.log(responseData.nicFrontByte)
+            console.log(responseData.nicRearByte)
+            console.log(responseData.profilePic)
+            console.log(responseData.nicFront)
+            console.log(responseData.nicRear)
+
+            $('#userName').val(responseData.username);
+            $('#nicNo').val(responseData.usernic);
+            $('#contact').val(responseData.contact);
+            $('#email').val(responseData.email);
+            $('#birthday').val(responseData.birthday);
+
+            console.log(responseData.birthday)
+            $('#nicFront-img-show').attr('src', "data:image/jpeg;base64," + responseData.nicFront); // Set the src attribute for an image element
+            $('#nicRear-img-show').attr('src', "data:image/jpeg;base64," + responseData.nicRear);   // Set the src attribute for an image element
+           $('#gender').val(responseData.gender)
+            //$('#remarks').text(responseData.remarks);
+            $('#profilePic-img-show').attr('src', "data:image/jpeg;base64," + responseData.profilePic); // Set the src attribute for an image element
+
+
+        });
+    }
+
 
     saveUser() {
         let userName = $('#userName').val();
@@ -19,7 +68,7 @@ export class UserController {
         var profilePic = $('#profilePic').prop('files')[0];
         var nicFront = $('#nicFront').prop('files')[0];
         var nicRear = $('#nicRear').prop('files')[0];
-        let nicNo = $("nicNo").val();
+        let nicNo = $("#nicNo").val();
 
 
         console.log(userName)
@@ -46,8 +95,10 @@ export class UserController {
             form.append("profilePic", profilePic, "ab.jpg");
         if (nicFront)
             form.append("nicFront", nicFront, "aa.jpg");
-        if (nicNo)
-            form.append("nicRear", nicNo, "ac.jpg");
+        if (nicRear)
+            form.append("nicRear", nicRear, "ac.jpg");
+
+
         form.append("nicNo", nicNo);
 
 
@@ -56,7 +107,7 @@ export class UserController {
             "method": "POST",
             "timeout": 0,
             "headers": {
-                "Authorization": "Bearer " + getKey()
+                "Authorization": "Bearer " + getKey(),
             },
             "processData": false,
             "mimeType": "multipart/form-data",
@@ -66,6 +117,8 @@ export class UserController {
 
         $.ajax(settings).done(function (response) {
             console.log(response);
+        }).fail(e => {
+            console.log(e.code)
         });
 
 
